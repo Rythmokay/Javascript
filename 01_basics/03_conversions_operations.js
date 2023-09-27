61e5ac37c6573d48ef38d9ed0ab64ac201bd0775
Rythmokay <129789738+Rythmokay@users.noreply.github.com>
2023-09-27 20:43:15 +0000

    conversation of datatypes

---
+++ 01_basics/03_conversions_operations.js
@@ -0,0 +1,41 @@
+// let score = true;
+// console.log(typeof score);
+// console.log(typeof { score });
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// // "33" => 33
+// // "33abc"=>NaN (not a number)
+// // true => 1; false =>0
+// let isLoggedIn = 1;
+// let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
+// "" = false 
+// let someNUmber=33;
+// let stringNUmber =String(someNUmber); 
+// console.log(stringNUmber)
+// operations
+// let value=3
+// let negaValue=-value 
+// console.log(negaValue); 
+// Arithmetic operation 
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**2);
+//  console.log(2/2);
+//  console.log(2%2);
+// let str1="hello"
+// let str2=" Rythm"
+// let str3=str1+str2;
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2 +2 );
+let num1,num2,num3;
+num1=num2=num3=2+2
+let gameCounter=100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
